d7a622aa510996dff22bb86f7a67d4d0a9a45259
tubasthebest <tubasthebest@gmail.com>
2020-12-05 21:12:04 -0700

    correct times

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var timeNow = moment().format('H');
 timeNow = parseInt(timeNow);
 
 var timeBlocks = [$('#block1'), $('#block2'), $('#block3'), $('#block4'), $('#block5'), $('#block6'), $('#block7'), $('#block8'), $('#block9')];
-var timeValues = [9, 10, 11, 12, 21, 22, 23, 24, 25];
+var timeValues = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 var tasks = [];
 
